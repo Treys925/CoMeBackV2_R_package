9d964d314fe84eb029f9813d0a6daaa3727a64fd
Treys925 <109106345+Treys925@users.noreply.github.com>
2025-02-18 13:22:50 -0500

    Add files via upload

--- R/comeback.R
+++ R/comeback.R
@@ -1,14 +1,14 @@
 #' @details Currently, only the cmr function is provided.
 #'
 #' @section cmr:
-#' The \code{cmr} function constructs comethylated adjacent probe sets with correlated methylation across individuals,
+#' The \code{cmr} function constructs co-methylated adjacent probe sets with correlated methylation across individuals,
 #' with correlation threshold depending on genomic CpG density between each two probes
 #'
 #'@section cmr_comp:
 #' The \code{cmr_comp} function constructs a composite methylation measure for each CMR
 #'
 #' @docType package
-#' @name comeback
+#' @name "CoMeBackV2"
 #'
 #' @keywords internal
 "_PACKAGE"
