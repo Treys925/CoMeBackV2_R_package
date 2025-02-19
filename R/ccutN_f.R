0badc1e62e6673e4ea7e58902e6c8951ef73f110
Treys925 <109106345+Treys925@users.noreply.github.com>
2025-02-19 11:00:18 -0500

    Add files via upload

---
+++ R/ccutN_f.R
@@ -0,0 +1,12 @@
+#' @title Correlation cut-off as function of sample size
+#'
+#' @description Empirical function based on simulation
+#'
+#' @param N is the sample size
+#' @return the correlation cut-off value
+#' @author Trey Smith, \email{treysm@@umich.edu}
+ccutN_f=function(N){
+  if (N<25) res=0.5
+  else res=0.05+0.45*10^(-((N-25)^0.46)/21) #max(0.1,0.38*10^(-((N-50)^0.48)/36))
+  return(res)
+}
