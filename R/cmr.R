23515426faec3a7deaa2e16b07c90d694278e68c
Treys925 <109106345+Treys925@users.noreply.github.com>
2025-02-28 15:45:42 -0500

    Update cmr.R
    Adding genome build hs1

--- R/cmr.R
+++ R/cmr.R
@@ -6,12 +6,13 @@
 #'
 #' @param Mdata matrix of betas, preferably cell-type corrected, or M-values, where rows are observations and colums are array probes.
 #' @param meds medians of probe betas, used if constraining adjacent probe maximum difference
-#' @param Iarray which Illumina array platform, one of "450K" (default), EPIC, or "EPICv2".
+#' @param Iarray which Illumina array platform, one of "450K" (default), "EPIC", or "EPICv2".
+#' @param Build which Genome build, one of "hg38" (default) or "hs1". NOTE: for hs1 all files from /data must be loaded into the global environment.
 #' @param cormethod a character string indicating which correlation is to be computed. One of "pearson" (default), "kendall", or "spearman".
-#' @param corlo low threshold for correlation, when max distance betweek adjacent genomic CpGs is corlodst; optional - default is NULL
-#' @param corhi high threshold for correlation, when max distance betweek adjacent genomic CpGs is corhidst; optional - default is NULL
-#' @param corlodst the maximum distance between CpGs considered, defines low CpG denisty, default is 400bp
-#' @param corhidst the minimum distance between CpGs considered, defines high CpG denisty, default is 1bp
+#' @param corlo low threshold for correlation, when max distance between adjacent genomic CpGs is corlodst; optional - default is NULL
+#' @param corhi high threshold for correlation, when max distance between adjacent genomic CpGs is corhidst; optional - default is NULL
+#' @param corlodst the maximum distance between CpGs considered, defines low CpG density, default is 400bp
+#' @param corhidst the minimum distance between CpGs considered, defines high CpG density, default is 1bp
 #' @param maxprbdst Maximum bp distance between adjacent probes, default is 2000bp
 #' @param maxdlvl the maximum difference between the medians of the adjacent probes
 #' @param verbose whether to report progress for chromosomes, default is TRUE
@@ -23,9 +24,10 @@
 #' @author Trey Smith, \email{treysm@@umich.edu}
 #' @importFrom stats cor
 #' @export cmr
-cmr=function(Mdata,meds=NULL,Iarray=c("450K", "EPIC", "EPICv2"),cormethod=c("pearson", "kendall", "spearman"), corlo=NULL,corhi=corlo,corlodst=400,corhidst=1, maxprbdst=2000, maxdlvl=Inf, verbose=TRUE){ #
-  # scan sequentially probes, get max CpG between them, and based on that estimate correlation or not,
-  # add next probe to current CMR if  correlation passes variable cutoff that depends on mac CpG gap
+cmr=function(Mdata,meds=NULL,Iarray=c("450K", "EPIC", "EPICv2"),Build = "hg38",cormethod=c("pearson", "kendall", "spearman"), 
+             corlo=NULL,corhi=corlo,corlodst=400,corhidst=1, maxprbdst=2000, maxdlvl=Inf, verbose=TRUE){ #
+  # scan probes sequentially, get the number of CpGs between them, and based on that estimate correlation or not,
+  # add next probe to current CMR if  correlation passes variable cutoff that depends on max CpG gap
 
   cormethod <- match.arg(cormethod)
   Iarray <- match.arg(Iarray)
@@ -37,11 +39,22 @@ cmr=function(Mdata,meds=NULL,Iarray=c("450K", "EPIC", "EPICv2"),cormethod=c("pea
 
 
   if (verbose) print("Getting CpG info, about to start estimation any day now.")
+  
+  if (Build == "hs1"){
+    chr_seq_GpCpos=backgroundProbes
+    if(Iarray=="450K") {EPIC_Manifest=Anno450k}
+    else if (Iarray == "EPIC"){EPIC_Manifest=AnnoEPIC}
+    else {EPIC_Manifest=AnnoEPICv2}
+  }
+    
+    else{
 
     chr_seq_GpCpos=CoMeBackV2:::init_data$chr_seq_GpCpos
     if(Iarray=="450K") {EPIC_Manifest=CoMeBackV2:::init_data$I450K_Manifest}
     else if (Iarray == "EPIC"){EPIC_Manifest=CoMeBackV2:::init_data$EPIC_Manifest}
     else {EPIC_Manifest=CoMeBackV2:::init_data$EPICv2_Manifest}
+    
+  }
 
     EPIC_OK_prb=rownames(EPIC_Manifest)
 
@@ -87,11 +100,11 @@ cmr=function(Mdata,meds=NULL,Iarray=c("450K", "EPIC", "EPICv2"),cormethod=c("pea
       # for each pair of consecutive probes in this chromosome, are they correlated above the variable cut-off
       ccut_tf=sapply(2:length(cpgi), function(y){ # for each pair of consecutive probes in this cluster
         res=FALSE # the correlation is above cutoff
-        # for this chromosomes's consecutive probes, all gaps between consecutive CpGs
+        # for this chromosomes' consecutive probes, all gaps between consecutive CpGs
         ym1=y-1
         dcpg=chr_seq_GpCpos[[cni]][(cpgi[[ym1]]+1):cpgi[[y]]]-chr_seq_GpCpos[[cni]][cpgi[[ym1]]:(cpgi[[y]]-1)]
         maxdcpg=max(dcpg)
-        # if the maximum CpG gap is below the window, evaluate the correlation and threshold, check if cor is below threshold
+        # if the maximum CpG gap is below the window, evaluate the correlation and check if cor is below threshold
         if((maxdcpg <= corlodst)&(abs(wkprbs_crd[[x[[(ym1)]]]]-wkprbs_crd[[x[[(y)]]]])<=maxprbdst)){
           corcut=corhi-(maxdcpg-corhidst)*(corhi-corlo)/(corlodst-corhidst) # the correlation cut-off for the pair of probes
           res=(cor(x=Mdata[,x[[(ym1)]]],y=Mdata[,x[[y]]],use = "pairwise.complete.obs", method = cormethod)>corcut)
@@ -99,7 +112,7 @@ cmr=function(Mdata,meds=NULL,Iarray=c("450K", "EPIC", "EPICv2"),cormethod=c("pea
         return(res)
       })
 
-      ccut_i=which(ccut_tf) # which paris of adjacent probes are correlated above cutoff
+      ccut_i=which(ccut_tf) # which pairs of adjacent probes are correlated above cutoff
       lpc=length(ccut_i)
 
       cmr1dcr=NULL
@@ -162,7 +175,7 @@ cmr=function(Mdata,meds=NULL,Iarray=c("450K", "EPIC", "EPICv2"),cormethod=c("pea
       # for each pair of consecutive probes in this chromosome, are they correlated above the variable cut-off
       ccut_tf=sapply(2:length(cpgi), function(y){ # for each pair of consecutive probes in this cluster
         res=FALSE # the correlation is above cutoff
-        # for this chromosomes's consecutive probes, all gaps between consecutive CpGs
+        # for this chromosomes' consecutive probes, all gaps between consecutive CpGs
         ym1=y-1
         dcpg=chr_seq_GpCpos[[cni]][(cpgi[[ym1]]+1):cpgi[[y]]]-chr_seq_GpCpos[[cni]][cpgi[[ym1]]:(cpgi[[y]]-1)]
         maxdcpg=max(dcpg)
@@ -174,7 +187,7 @@ cmr=function(Mdata,meds=NULL,Iarray=c("450K", "EPIC", "EPICv2"),cormethod=c("pea
         return(res)
       })
 
-      ccut_i=which(ccut_tf) # which paris of adjacent probes are correlated above cutoff
+      ccut_i=which(ccut_tf) # which pairs of adjacent probes are correlated above cutoff
       lpc=length(ccut_i)
 
       cmr1dcr=NULL
